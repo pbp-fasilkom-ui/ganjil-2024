74be35b4c034791fb5e13e71bdd3cfdda91791a5
Nicholas Sidharta <toxicancm@gmail.com>
2023-06-16 17:02:25 +0700

    Add Styling to cards

--- src/components/QuickAccess/QuickAccessCard/index.js
+++ src/components/QuickAccess/QuickAccessCard/index.js
@@ -13,18 +13,23 @@ const Index = ({
   const handleMouseOut = () => setIsHovered(false);
 
   return (
-    <a href={target} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+    <a
+      href={target}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+      className="text-black no-underline"
+    >
       <div className="flex flex-col h-64 w-full max-w-64 bg-gray-200 text-black rounded-xl overflow-hidden transition duration-200 ease-in-out transform hover:scale-105">
         <div className={isHovered ? "h-0 hidden" : "h-2/4 flex"}>
-          <img src={image} className="object-cover w-full h-auto" />
+          <img src={image} className="object-cover w-full h-auto" alt="Card Image" />
         </div>
         <div className="flex flex-col gap-1 p-4">
-          <h3 className="font-semibold ">{title}</h3>
+          <h3 className="font-semibold text-xl">{title}</h3>
           <span className="italic">{subtitle}</span>
           <p
             className={
               isHovered
-                ? "text-elipsis overflow-clip h-full line-clamp-6 break-words"
+                ? "text-ellipsis overflow-clip h-full line-clamp-6 break-words"
                 : "hidden"
             }
           >
@@ -33,7 +38,7 @@ const Index = ({
         </div>
       </div>
     </a>
-  );
+  );  
 };
 
 export default Index;
